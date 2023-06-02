7b512b82f0597e2d7957304a161c39a27e707c8c
@hexlet/cli <support@hexlet.io>
2023-06-02 21:24:35 +0300

    submit java-oop-ru/interfaces

---
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -0,0 +1,17 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+    public static List<String> buildApartmentsList(List<Home> homes, int n) {
+        if (n > homes.size()) {
+            return new ArrayList<>();
+        }
+        List<String> result = new ArrayList<>(homes.stream().sorted().map(Object::toString).toList());
+
+        return result.subList(0, n);
+    }
+}
+// END
